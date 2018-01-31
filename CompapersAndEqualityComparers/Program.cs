70c20d15ca407572179ef4de17937274a4203668
ikostan <igorkostan@gmail.com>
2018-01-31 14:11:35 -0800

    Compapers and Equality Comparers

---
+++ CompapersAndEqualityComparers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompapersAndEqualityComparers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compapers and Equality Comparers:\n");
+
+           
+
+            Console.WriteLine("\nPlease enter any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
