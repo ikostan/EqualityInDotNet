fd4ab37ad337a021326664f030203f5fe10a515e
ikostan <igorkostan@gmail.com>
2018-01-31 20:34:07 -0800

    WorkingWithStructuralEquality

---
+++ WorkingWithStructuralEquality/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStructuralEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working With Structural Equality:\n");
+
+            try
+            {
+                //
+                
+
+            }
+            catch (Exception c)
+            {
+                Console.WriteLine(c.Message);
+                Console.WriteLine(c.StackTrace);
+            }
+
+            Console.WriteLine("\nPlease enter any key to exit...");
+            Console.ReadKey();
+        }
+
+        //End of Class
+    }
+}
